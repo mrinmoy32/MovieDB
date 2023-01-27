549bd0fe6c4b0974cef84b9a8d583a11f549b939
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-01-27 20:00:54 +0530

    movie_poster_api

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<h1>Movie Details Page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie List Page</h1>}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css";
+import {Link} from "react-router-dom";
+import image from "./icon.jpg";
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='headerLeft'>
+            <Link to="/"><img className='header_icon' src={image} /></Link>
+            <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+            <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+            <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Header
